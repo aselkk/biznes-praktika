89c0847410cec46fef0e647f8ce1db08cc264a90
Asel <aselkurmanbekk@gmail.com>
2022-03-22 16:41:23 +0600

    init

---
+++ .anaconda/navigator/scripts/notebook.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+source /anaconda3/bin/activate /anaconda3
+open /anaconda3/bin/jupyter_mac.command >/Users/aselya/.anaconda/navigator/scripts/notebook-out-1.txt 2>/Users/aselya/.anaconda/navigator/scripts/notebook-err-1.txt
---
+++ .bash_profile
@@ -0,0 +1,3 @@
+
+# added by Anaconda3 5.2.0 installer
+export PATH="/anaconda3/bin:$PATH"
